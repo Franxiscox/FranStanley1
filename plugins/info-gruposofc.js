3ef4169ea62fb584bff09b57ac7d588e21f9a912
Fran Stanley <89065588+Franxiscox@users.noreply.github.com>
2022-09-17 23:46:44 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,46 +1,26 @@
 let media = './media/menus/Menuvid3.mp4'
 let handler = async (m, { conn, command }) => {
 let str = `
-✨ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝙊𝙎 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+✨ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼𝙇 𝙂𝙍𝙐𝙋𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-𝙏𝙚 𝙞𝙣𝙫𝙞𝙩𝙤 𝙖 𝙦𝙪𝙚 𝙩𝙚 𝙪𝙣𝙖𝙨 𝙖 𝙡𝙖 𝘾𝙤𝙢𝙪𝙣𝙞𝙙𝙖𝙙 𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫. ✨ 𝙏𝙚𝙣 𝙪𝙣 𝙗𝙪𝙚𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤 𝙚 𝙞𝙣𝙩𝙚𝙧𝙖𝙘𝙘𝙞𝙤𝙣𝙖 𝙘𝙤𝙣 𝙉𝙤𝙨𝙤𝙩𝙧𝙤𝙨. 😸
+𝙏𝙚 𝙞𝙣𝙫𝙞𝙩𝙤 𝙖 𝙦𝙪𝙚 𝙩𝙚 𝙪𝙣𝙖𝙨 𝙖 𝙡𝙖 𝘾𝙤𝙢𝙪𝙣𝙞𝙙𝙖𝙙 𝕭𝖔𝖙 𝕱𝖗𝖆𝖓 𝕾𝖙𝖆𝖓𝖑𝖊𝖞ت︎ツ. ✨ 𝙏𝙚𝙣 𝙪𝙣 𝙗𝙪𝙚𝙣 𝙢𝙤𝙢𝙚𝙣𝙩𝙤 𝙚 𝙞𝙣𝙩𝙚𝙧𝙖𝙘𝙘𝙞𝙤𝙣𝙖 𝙘𝙤𝙣 𝙉𝙤𝙨𝙤𝙩𝙧𝙤𝙨. 😸
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 𓃠 *Versión de ${gt}*
 ➥ ${vs}
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ ➤ 𝙂𝙧𝙪𝙥𝙤𝙨 𝙤𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 𝙙𝙚𝙡 𝘽𝙤𝙩:
+✅ ➤ 𝙂𝙧𝙪𝙥𝙤 𝙤𝙛𝙞𝙘𝙞𝙖𝙡 𝙙𝙚𝙡 𝘽𝙤𝙩:
 1) *${nn}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 2) *${nnn}*\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-3) *https://chat.whatsapp.com/Byyrc8RJnUkJw6vvUgsbDF*
+3) *http://instagram.com/franx_xiscox*
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*grupo del ayuda sobre el bot*
-*https://chat.whatsapp.com/HBG6B75TTnx21a0P3ueQ01*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
- 𝙜𝙧𝙪𝙥𝙤 𝙙𝙚 𝙖𝙢𝙞𝙨𝙩𝙖𝙙𝙚𝙨 𝙙𝙤𝙣𝙙𝙚 𝙚𝙨𝙩𝙖 𝙚𝙡 𝙗𝙤𝙩
-
-*ஓீ͜ঔৣ͡🍒𝑪𝑬̈𝑹𝑬𝒁𝑨̈.𝑩Ø𝑻🔥ঔৣ͡ஓ*
-https://chat.whatsapp.com/DCJclB8oBAPIAoleUtNEaN
-
-*꧁⁣𓆩Aᴹᴵᴳᴼˢ Cᴴᴬᵀˢ Y Aᴾᴼᴿᵀᴱ*
-https://chat.whatsapp.com/JZaD3sfNoVW4JvaoQ4uVwF
-
 𝐚𝐪𝐮𝐢 𝐩𝐮𝐞𝐝𝐞 𝐦𝐚𝐧𝐝𝐚 𝐭𝐮 𝐞𝐧𝐥𝐚𝐜𝐞 𝐲 𝐭𝐚𝐦𝐛𝐢𝐞́𝐧 𝐬𝐢 𝐪𝐮𝐢𝐞𝐫𝐞 𝐮𝐧 𝐛𝐨𝐭 𝐩𝐚𝐫𝐚 𝐭𝐮 𝐠𝐫𝐮𝐩𝐨 𝐩𝐨𝐧𝐞𝐫 𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨 .𝐣𝐨𝐢𝐧 (𝐦𝐚𝐬 𝐞𝐧𝐥𝐚𝐜𝐞)
-
-*⇶⃤꙰𝑬𝒏𝒍𝒂𝒄𝒆 𝒍𝒐𝒍𝒊𝒃𝒐𝒕ꦿ⃟⃢*
-https://chat.whatsapp.com/CSYqcJOZuK9Kff7eoAUAqd
-
-*꧁⃢⃟҉💫🌺ঔঔ𝔼ℕ𝕃𝔸ℂ𝔼𝕊 𝕃𝔸 𝕁𝔼𝔽𝔸✨*
-https://chat.whatsapp.com/JegOp8NUSMd0vn75s4hkaj
-
-*℻⃟⚔️𝐿𝑒𝑔𝑖ó𝑛 𝐴𝑛𝑖𝑚𝑒⻝ღ*
-https://chat.whatsapp.com/KbDiJuy4qLAIWSchpj3jCa\n
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
  𝙨𝙞 𝙦𝙪𝙞𝙚𝙧𝙚 𝙦𝙪𝙚 𝙩𝙪 𝙜𝙧𝙪𝙥𝙤 𝙖𝙥𝙖𝙧𝙚𝙯𝙘𝙖 𝙖𝙘𝙖 𝙝𝙖𝙗𝙡𝙖 𝙘𝙤𝙣 𝙚𝙡 𝙖𝙙𝙢𝙞𝙣 𝙤𝙛𝙘 𝙙𝙚𝙡 𝙗𝙤𝙩
 `.trim()
   
-conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/elrebelde21/The-LoliBot-MD', '𝑻𝒉𝒆 𝑳𝒐𝒍𝒊𝑩𝒐𝒕-𝑴𝑫', null, null, [
+conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/Franxiscox', '𝕭𝖔𝖙 𝕱𝖗𝖆𝖓 𝕾𝖙𝖆𝖓𝖑𝖊𝖞ت︎ツ', null, null, [
 ['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 ✅', '.cuentasgb'],
 ['🎁 𝘿𝙤𝙣𝙖𝙧', '.donar'],
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', '/menu']
