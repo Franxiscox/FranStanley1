2ab1acc320cda53d45e3edb096ff2d0410c9f29e
Fran Stanley <89065588+Franxiscox@users.noreply.github.com>
2022-09-21 02:43:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -80,8 +80,8 @@ global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 
 //⊱ ━━━━━.⋅ Sticker WM ⋅.━━━━ ⊰
 
-global.packname = 'Wa.me/+595984101796'
-global.author = '𝕭𝖔𝖙 𝕱𝖗𝖆𝖓 𝕾𝖙𝖆𝖓𝖑𝖊𝖞ت︎ツ'
+global.packname = '𝕭𝖔𝖙 𝕱𝖗𝖆𝖓 𝕾𝖙𝖆𝖓𝖑𝖊𝖞ت︎ツ'
+global.author = 'wa.me/+595984101796'
 
 //━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
