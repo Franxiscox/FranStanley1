cea34088a026ea0a652fad3ecbbd6562ad825005
Fran Stanley <89065588+Franxiscox@users.noreply.github.com>
2022-09-21 13:56:40 -0400

    Update rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,9 +1,10 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`⬇️ *ESE ES SU NUMERO DE SERIE* ⬇️`.trim())
-await m.reply(`${sn}`.trim())
-}
+m.reply(`┏┅ ━━━━━━━━━━━━ ┅ ━
+┃ *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
+┃ ${sn}
+┗┅ ━━━━━━━━━━━━ ┅ ━`.trim())}
 handler.help = ['myns']
 handler.tags = ['xp']
 handler.command = /^(myns|ceksn)$/i
